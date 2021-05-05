2075e1d67b884aa3504b852a6decadee686814ab
di2mot <motsar.dm@gmail.com>
2021-05-05 23:27:35 +0300

    Update views.py

--- bboard/views.py
+++ bboard/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 def index(request):
 
-    return HttpResponse(f'Here will be  more text soon\n Request:\b {request.META}')
+    return HttpResponse('Here will be  more text soon')
