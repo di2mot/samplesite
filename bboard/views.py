02fc2fb29807b5e1bf4f48bbe308ddb9b20bb3f6
di2mot <motsar.dm@gmail.com>
2021-05-06 23:45:38 +0300

    Update views.py

--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from .models import Bd
 
 def index(request):
+    s = 'Same text\r\n\r\n\r\n'
 
-    return HttpResponse('Here will be  more text soon')
+    for bb in Bd.objects.order_by('-published'):
+        s += bb.title + '\r\n' + bb.content + '\r\n'
+
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
