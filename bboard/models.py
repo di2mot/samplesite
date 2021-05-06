51be52decffd0721b626285d4e06840d700c7322
di2mot <motsar.dm@gmail.com>
2021-05-06 23:19:26 +0300

    upd

--- bboard/models.py
+++ bboard/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Bd(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+    published = models.DateTimeField(auto_now=True, db_index=True)
+
+
+
+
+
 # Create your models here.
