ce0ef5c3323e35850ec36b265d1d278fe1be4af1
di2mot <motsar.dm@gmail.com>
2021-05-16 22:44:12 +0300

    Update views.py

--- bboard/views.py
+++ bboard/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from .models import Bb
-import django.template.loader as loader
+# import django.template.loader as loader
+from django.shortcuts import render
+
+# def index(request):
+#     template = loader.get_template('bboard/index.html')
+#     bbs = Bb.objects.order_by('-published')
+#     context = {'bbs': bbs}
+#     return HttpResponse(template.render(context, request))
 
 def index(request):
-    template = loader.get_template('bboard/index.html')
     bbs = Bb.objects.order_by('-published')
-    context = {'bbs': bbs}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'bboard/index.html', {'bbs': bbs})
