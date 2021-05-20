3fe4c8d593ae19f759e0d46141a2d7d6b53b42c8
di2mot <motsar.dm@gmail.com>
2021-05-20 22:04:45 +0300

    hm, it work`s

--- samplesite/urls.py
+++ samplesite/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 from bboard.views import index, by_rubric
 
 urlpatterns = [
-    path('<int:rubric_id>/', by_rubric),
-    path('', index),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('bboard/', index),
 ]
